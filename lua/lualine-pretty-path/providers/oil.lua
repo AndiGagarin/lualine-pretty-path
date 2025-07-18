fe372450e2465aa90e065aeaa4075215c5f622c1
Andreas Gahr <Andreas.GB.Gahr@bmw.de>
2025-07-19 01:19:19 +0200

    render the whole oil directory, not just the parent directory

--- lua/lualine-pretty-path/providers/oil.lua
+++ lua/lualine-pretty-path/providers/oil.lua
@@ -1,3 +1,4 @@
+local utils = require("lualine-pretty-path.utils")
 ---@class PrettyPath.OilProvider: PrettyPath.Provider
 ---@field super PrettyPath.Provider
 local M = require("lualine-pretty-path.providers.base"):extend()
@@ -6,14 +7,10 @@ function M.can_handle()
     return vim.bo.filetype == "oil"
 end
 
-function M:format_path(_)
-    return require("oil").get_current_dir()
-end
-
 ---Renders the oil current directory as if it was the "filename" part.
 ---@return string
 function M:render_dir()
-    local dir = self.super.render_dir(self) or ""
+    local dir = require("oil").get_current_dir() or ""
     dir = dir:sub(1, #dir - #self.path_sep) -- remove trailing separator
 
     local hl = self.opts.highlights.filename
@@ -26,6 +23,7 @@ end
 
 function M:render()
     return table.concat({
+        self:extract_scheme() or "",
         self:render_dir() or "",
         self:render_symbols() or "",
     }, "")
