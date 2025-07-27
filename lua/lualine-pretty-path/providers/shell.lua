cb1a3098e28db8985c2fa76e6aef5ecc77f1760b
Andreas Gahr <Andreas.GB.Gahr@bmw.de>
2025-07-27 23:29:20 +0200

    toy provider for my poor man shell

---
+++ lua/lualine-pretty-path/providers/shell.lua
@@ -0,0 +1,32 @@
+local utils = require("lualine-pretty-path.utils")
+---@class PrettyPath.ShellProvider: PrettyPath.Provider
+---@field super PrettyPath.Provider
+local M = require("lualine-pretty-path.providers.base"):extend()
+
+function M.can_handle()
+    return vim.bo.filetype == "shell"
+end
+
+---Renders the oil current directory as if it was the "filename" part.
+---@return string
+function M:render_dir()
+    local name = vim.api.nvim_buf_get_name(0)
+    name = vim.split(name, "//") [1]
+    name  = name .. "//" .. vim.api.nvim_buf_get_var(0, "term_title")
+    local hl = self.opts.highlights.filename
+
+    return self.hl(name, hl)
+end
+
+function M:get_icon()
+    return { utils.get_icon("terminal") }
+end
+
+function M:render()
+    return table.concat({
+        self:render_dir() or "",
+        --self:render_symbols() or "",
+    }, "")
+end
+
+return M
